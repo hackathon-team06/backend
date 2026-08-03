85522d07d540bb410969ef7912fe7a3550423804
namkungkang <namkung0131@naver.com>
2026-08-03 14:41:25 +0900

    first commit

---
+++ src/main/java/com/likelion/staycare/StayCareApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.staycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/staycare/StayCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.staycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
